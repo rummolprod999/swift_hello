0f2f08fefac21ccaedb94a257ce9d9d37a9fc7ba
rummolprod999 <rummolprod999@gmail.com>
2019-03-01 14:44:35 +0300

    update

--- Sources/swift_hello/main.swift
+++ Sources/swift_hello/main.swift
@@ -4,10 +4,26 @@ let str = "x1 + x2 = \((x1 + x2))"
 print(str)
 let a = Int("123")
 let b = Int("456")
-if let aVal = a, let bVal = b{
+if let aVal = a, let bVal = b {
     print(aVal)
     print(bVal)
-}
-else{
+} else {
     print("Error")
 }
+
+let hello = {
+    print("Hello world")
+}
+hello()
+hello()
+
+func action() -> (()->Int){
+
+    var val = 0
+    return { () -> Int in
+        val = val+1
+        return val
+    }
+}
+let inc = action()
+print(inc())
